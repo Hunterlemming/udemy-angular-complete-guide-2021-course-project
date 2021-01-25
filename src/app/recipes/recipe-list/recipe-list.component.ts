90d995976b2fed308d8eda9a568611488de20567
Krisztian Kertesz <kkertesz@precognox.com>
2021-01-25 14:49:10 +0100

    Outputting a List of Recipies with ngFor

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,8 +9,8 @@ import {Recipe} from '../recipe.model';
 export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[] = [
-    new Recipe('Test Recipe', 'This is a test', 'https://static.wikia.nocookie.net/shokugekinosoma/images/b/b0/' +
-      'Roast_Pork%2C_Just_Kidding_%28anime%29.png/revision/latest?cb=20150419102847')
+    new Recipe('Test Recipe', 'This is a test', 'https://fandomeducationinnovation.files.wordpress.com/2019/02/food-wars-saumon-confit-flamme.png'),
+    new Recipe('Test Recipe', 'This is a test', 'https://fandomeducationinnovation.files.wordpress.com/2019/02/food-wars-saumon-confit-flamme.png')
   ];
 
   constructor() { }
