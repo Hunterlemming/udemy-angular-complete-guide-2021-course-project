21fd2dbdeddcfc5922bed3dbc388199f4ecd8b43
Krisztian Kertesz <kkertesz@precognox.com>
2021-01-25 14:11:16 +0100

    52. Adding Content to the Recipes Components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'This is a test', 'https://static.wikia.nocookie.net/shokugekinosoma/images/b/b0/' +
+      'Roast_Pork%2C_Just_Kidding_%28anime%29.png/revision/latest?cb=20150419102847')
+  ];
 
   constructor() { }
 
