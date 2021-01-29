b232f6e5e1fe67b9a15cd6ceec0960df34940e67
Krisztian Kertesz <kkertesz@precognox.com>
2021-01-29 11:09:02 +0100

    121 Adding Ingredients to Recipes

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,14 +1,17 @@
+import {Ingredient} from '../shared/ingredient.model';
 
 export  class Recipe {
 
   public recName: string;
   public description: string;
   public imagePath: string;
+  public ingredients: Ingredient[];
 
-  constructor(recName: string, description: string, imagePath: string) {
+  constructor(recName: string, description: string, imagePath: string, ingredients: Ingredient[]) {
     this.recName = recName;
     this.description = description;
     this.imagePath = imagePath;
+    this.ingredients = ingredients;
   }
 
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import {EventEmitter, Injectable} from '@angular/core';
 import {Recipe} from './recipe.model';
+import {Ingredient} from '../shared/ingredient.model';
 
 @Injectable({
   providedIn: 'root'
@@ -7,8 +8,18 @@ import {Recipe} from './recipe.model';
 export class RecipeService {
 
   private recipes: Recipe[] = [
-    new Recipe('Test Recipe', 'This is a test', 'https://fandomeducationinnovation.files.wordpress.com/2019/02/food-wars-saumon-confit-flamme.png'),
-    new Recipe('Another Test Recipe', 'This is a test', 'https://fandomeducationinnovation.files.wordpress.com/2019/02/food-wars-saumon-confit-flamme.png')
+    new Recipe('Schnitzel', 'This is a Schnitzel.',
+      'https://www.thespruceeats.com/thmb/cckc3_4QUQ79kSFhcLPM8xg9F3g=/3797x2848/smart/filters:no_upscale()/wiener-schnitzel-recipe-1447089-Hero-5b587d6c46e0fb0071b0059d.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]),
+    new Recipe('Hamburger', 'This is a Hamburger.',
+      'https://www.mindmegette.hu/images/155/O/crop_201606161937_hamburger.jpg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1)
+      ])
   ];
   recipeSelected = new EventEmitter<Recipe>();
 
